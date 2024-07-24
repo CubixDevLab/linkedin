35b2cb664aafa63c6fef01be41f5f12757f7354a
Tahir Abbas <tahir.abbas@cubixlabs.com>
2024-07-24 17:11:00 +0500

    inital commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
@@ -286,8 +287,27 @@ module.exports = {
         'spin-1.5': 'spin 1.5s linear infinite',
         'spin-2': 'spin 2s linear infinite',
         'spin-3': 'spin 3s linear infinite',
+=======
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+>>>>>>> 9e2149a (inital commit)
       },
     },
   },
   plugins: [],
+<<<<<<< HEAD
 }
+=======
+};
+>>>>>>> 9e2149a (inital commit)
